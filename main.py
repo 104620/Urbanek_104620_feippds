38ab976024805adf49e660ea8eace3cdd3dc5d16
Paľko Urbanek <palkourbanek45@gmail.com>
2023-02-26 22:58:18 +0100

    feat: test commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""This module contains an implementation of Bakery algorithm.
+
+There are extensions of the Bakery algorithm for more than 2 threads,
+however this implementation works for only 2 threads.
+"""
+
+__author__ = "Paljko Urbanek"
+__email__ = "xurbanek@stuba.sk"
+__license__ = "MIT"
+
